fad38d628ed9583e68bca8017672ae1f2051e7fa
Gurjant Singh <gurjant245singh@gmail.com>
2025-05-06 23:00:55 +0200

    Version inicial

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "G-Books",
+  description: "Panjabi library in Valencia",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-700 text-slate-900 tracking-tight`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
